d5282cde433c2dbbef9cc2c9d77d81d37534b3de
Travis Haby <travishaby@gmail.com>
2020-08-16 17:26:40 -0600

    Working form submits new topics to the BE canister :D

--- src/message_board_assets/public/index.jsx
+++ src/message_board_assets/public/index.jsx
@@ -8,6 +8,7 @@ class MessageBoard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      newTopic: {},
       topics: []
     };
   }
@@ -19,6 +20,16 @@ class MessageBoard extends React.Component {
   async getTopics() {
     const topics = await message_board.getTopics();
     this.setState({ ...this.state, topics: topics });
+  }
+
+  handleInputChange({target: {value, name}}) {
+    this.setState(oldState => ({ newTopic: { ...oldState.newTopic, [name]: value}}))
+  }
+
+  async handleCreate(submitEvent) {
+    submitEvent.preventDefault()
+    const { title, description } = this.state.newTopic
+    await message_board.addTopic(title, description)
   }
 
   renderTopics() {
@@ -35,6 +46,15 @@ class MessageBoard extends React.Component {
       <div>
         <div class="message-board-title">
           <h1>Greetings, to the Guild DApp Message Board!</h1>
+        </div>
+        <div class="new-message">
+          <form>
+            <label htmlFor="title-input">Title</label>
+            <input onChange={this.handleInputChange.bind(this)} name="title" id="title-input" type="text" placeholder="Your topic's title"/>
+            <label htmlFor="description-input">Description</label>
+            <input onChange={this.handleInputChange.bind(this)} name="description" id="description-input" type="text" placeholder="Your topic's description"/>
+            <button onClick={this.handleCreate.bind(this)}>Create</button>
+          </form>
         </div>
         <div>
           {this.renderTopics()}
